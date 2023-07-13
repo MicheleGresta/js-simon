690cd18e6e738440b862335adec5b44b0a1aa314
MicheleGresta <malachia_1997@hotmail.it>
2023-07-13 17:34:03 +0200

    waiting for Alessio

---
+++ script/script.js
@@ -0,0 +1,37 @@
+const countdownElement = document.querySelector(".countdown");
+
+const countdown = () => {
+    const countDate = new Date("July 14, 2023 18:00:00").getTime();
+    const now = new Date().getTime();
+    const diff = countDate - now;
+
+
+    // trasformo i millesimi nelle varie ore minuti secondi
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    // dalla rimanenza del tempo (diff) prendo il resto e lo difivo in ore, poi il resto delle ore in minuti e cosi via
+
+    const textDay = Math.floor(diff / day);
+    console.log(textDay);
+    const textHour = Math.floor((diff % day) / hour);
+    const textMinute = Math.floor((diff % hour) / minute);
+    const textSecond = Math.floor((diff % minute) / second);
+
+    document.querySelector(".day").innerHTML = textDay;
+    document.querySelector(".hour").innerHTML = textHour;
+    document.querySelector(".minute").innerHTML = textMinute;
+    document.querySelector(".second").innerHTML = textSecond;
+
+
+    if (diff < 1) {
+        countdownElement.classList.add("display-none");
+    }
+}
+
+setInterval(countdown, 1000);
+
+
